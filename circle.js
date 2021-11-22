d30ea4ec1c7adab811fca414ec1bc462ff89ca87
KereBere <alsydm091@gmail.com>
2021-11-22 19:51:01 +0300

    First & Last Commit

---
+++ circle.js
@@ -0,0 +1,13 @@
+module.exports = {
+    circleArea,
+    circleCircumference,
+}
+
+function circleArea(radius) {
+    console.log(Math.PI * radius * radius)
+}
+function circleCircumference(radius) {
+    console.log(2 * Math.PI * radius)
+}
+
+
